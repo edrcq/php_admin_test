f3e6508e8f27aa9b9d2ed4e5c732ec327b4d6437
erucquoy@gmail.com <erucquoy@gmail.com>
2022-10-21 15:48:23 +0200

    update without story 4

--- actions/create_user.php
+++ actions/create_user.php
@@ -5,4 +5,22 @@ require_once __DIR__ . '/../init/db.php';
 // verifier les champs recu avec $_POST
 // Creer en BDD
 
+if (!isset($_POST['username'], $_POST['password'])) {
+    header('Location: ../users.php?error=create_user_empty');
+    die();
+}
+
+$username = $_POST['username'];
+$password = hash('sha256', $_POST['password']);
+
+if (strlen($username) < 2) {
+    header('Location: ../users.php?error=create_user_short_username');
+    die();
+}
+
+$stmt = $db->prepare('INSERT INTO users (username, password) VALUES(?, ?)');
+$stmt->execute([$username, $password]);
+
+header('Location: ../users.php?success_create=ok')
+
 ?>
--- users.php
+++ users.php
@@ -8,11 +8,11 @@ if (isset($_GET['username'])) {
 }
 
 // Story 0: request to find all username
-/*
-$stmt = ... 
+
+$stmt = $db->prepare('SELECT * FROM users');
 $stmt->execute();
 $users = $stmt->fetchAll();
-*/
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -21,12 +21,83 @@ $users = $stmt->fetchAll();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Liste des utilisateurs</title>
+    <style>
+        .flex-ct {
+            display: flex;
+            flex-direction: column;
+        }
+
+        .flex-row: {
+            margin-top: 20px;
+            margin-bottom: 20px;
+            display: row;
+        }
+
+        .flex-row a {
+            background-color: grey;
+            color: white;
+            padding: 3px;
+            margin: 3px;
+        }
+
+        .flex-row a:visited {
+            background-color: grey;
+            color: yellow;
+        }
+
+        .error {
+            background-color: red;
+        }
+
+        .success {
+            background-color: green;
+        }
+    </style>
 </head>
 <body>
+    <!-- Message erreur et success -->
+    <?php 
+    if (isset($_GET['success_delete'])) { ?>
+    <div class="flex-ct success">
+        <p>User deleted</p>
+    </div>
+    <?php
+    }
+    if (isset($_GET['success_create'])) { ?>
+    <div class="flex-ct success">
+        <p>User created</p>
+    </div>
+    <?php } ?>
+
+    <div>
+        <h2>Creer un user</h2>
+        <form method="POST" action="actions/create_user.php">
+            username: <input type="text" name="username" /><br />
+            password : <input type="password" name="password" /><br />
+            <input type="submit" value="Create now!">
+        </form>
+    </div>
+
     <!-- Input Search -->
-    <div></div>
+    <div>
+        <h2>Chercher un user</h2>
+        <form action="users.php" method="GET">
+            Username: <input type="text" name="id"><input type="submit" value="Search" />
+        </form>
+    </div>
 
     <!-- Table des Utilisateurs -->
-    <div></div>
+    <div class="flex-ct">
+        <h2>Tableau des users</h2>
+        <?php for($i = 0; $i < count($users); $i++) { ?>
+        <div class="flex-row">
+            <div>
+                User: <?= $users[$i]['username'] ?>
+                <a href="update_form.php?id=<?= $users[$i]['id'] ?>">Edit</a>
+                <a href="actions/delete_user.php?id=<?= $users[$i]['id'] ?>">Delete</a>
+            </div>
+        </div>
+        <?php } ?>
+    </div>
 </body>
 </html>
